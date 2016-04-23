33c26fa5c261d27b1db168be8e7daa963a73e65a
Alexandr Dragin <one.eastside@gmail.com>
2016-04-23 19:05:53 +0300

    new build

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -1,1 +1,51 @@
-var cbpAnimatedHeader=function(){function e(){window.addEventListener("scroll",function(e){c||(c=!0,setTimeout(n,100))},!1)}function n(){var e=t();e>=i?classie.add(r,"navbar-shrink"):classie.remove(r,"navbar-shrink"),a.style.opacity=1-e/100,c=!1}function t(){return window.pageYOffset||o.scrollTop}var o=document.documentElement,r=document.querySelector(".navbar-default"),a=document.querySelector(".arrow"),c=!1,i=700;a.style.opacity=.9,e()}();
+/**
+ * cbpAnimatedHeader.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2013, Codrops
+ * http://www.codrops.com
+ */
+var cbpAnimatedHeader = (function() {
+
+	var docElem = document.documentElement,
+		header = document.querySelector( '.navbar-default' ),
+		arrow1 = document.querySelector( '.arrow' ),
+		didScroll = false,
+		changeHeaderOn = 550;
+		arrow1.style.opacity = 0.9;
+
+	function init() {
+		window.addEventListener( 'scroll', function( event ) {
+			if( !didScroll ) {
+				didScroll = true;
+				setTimeout( scrollPage, 100 );
+			}
+		}, false );
+	}
+
+	function scrollPage() {
+		var sy = scrollY();
+		if ( sy >= changeHeaderOn ) {
+			classie.add( header, 'navbar-shrink' );
+		}
+		else {
+			classie.remove( header, 'navbar-shrink' );
+		};
+		//classie.add( arrow1, 'hidden');
+		arrow1.style.opacity = 1 - sy / 100 ;
+
+		didScroll = false;
+	}
+
+	function scrollY() {
+		return window.pageYOffset || docElem.scrollTop;
+	}
+
+
+
+	init();
+
+})();
